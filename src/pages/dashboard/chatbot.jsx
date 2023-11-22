3fa229d23b0fb6a8ad871446afec5af75eeb3ed7
aidenwong812 <wongaiden812@gmail.com>
2023-11-22 09:59:53 +0900

    init commit

---
+++ src/pages/dashboard/chatbot.jsx
@@ -0,0 +1,220 @@
+import React from "react";
+
+import {
+  Typography,
+  Card,
+  CardHeader,
+  CardBody, Input, Avatar, IconButton,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Button,
+} from "@material-tailwind/react";
+import { AnimationMessage } from "@/widgets/cards"
+import { Configuration, OpenAIApi } from 'openai';
+import 'dotenv'
+
+
+export function Chatbot() {
+
+
+  const API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
+
+  const configuration = new Configuration({
+    apiKey: API_KEY,
+  });
+
+  console.log('API_KEY>>>', API_KEY);
+
+  const openai = new OpenAIApi(configuration);
+
+  const [question, setQuestion] = React.useState("")
+  const [storedValues, setStoredValues] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+
+  const copyText = (text) => {
+    navigator.clipboard.writeText(text);
+  };
+
+  const handleSubmit = async (e, t) => {
+    e.preventDefault();
+    if (!question) {
+      alert("email required")
+      return;
+    }
+    let options = {
+      model: 'gpt-3.5-turbo',
+      temperature: 0,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ['/'],
+    };
+
+    let completeOptions = {
+      ...options,
+      messages: [{ role: 'user', content: t + question }],
+    };
+    setLoading(true);
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${t}${question}`,
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    });
+
+    if (response.data.choices[0].text) {
+      setStoredValues([
+        ...storedValues,
+
+        {
+          question: question,
+          answer: response.data.choices[0].text,
+        },
+      ]);
+      setQuestion("");
+    }
+    setLoading(false)
+  }
+
+  const handleMic = () => {
+    alert("clicked Mic!!!");
+  }
+
+  return (
+    <div className={`${loading ? "cursor-wait" : ""} mt-6 flex  flex-col gap-8 h-full`}>
+      <Card className="h-full">
+        <CardHeader
+          color="transparent"
+          floated={false}
+          shadow={false}
+          className="m-0 p-4"
+        >
+          <Typography variant="h4" color="blue-gray">
+            ChatGPT
+          </Typography>
+        </CardHeader>
+        <CardBody className="flex flex-col gap-4 p-4 px-10 grow">
+          <div className="overflow-y-auto grow">
+            {
+              storedValues.length == 0 ?
+                <div className="text-xl bg-blue-gray-800 mt-2 py-2 px-4 rounded-r-3xl rounded-bl-3xl text-white">I'm a ChatGPT, How can I assist you?</div>
+                :
+                storedValues.map((value, index) => {
+                  return (
+                    <div className="mt-2 pr-3" key={index}>
+                      <div className="flex flex-col items-end">
+                        <div className="flex flex-row">
+                          <div className="text-right text-xl bg-blue-400 text-white p-3 rounded-l-2xl rounded-br-2xl">
+                            {value.question}
+                          </div>
+                          <Avatar
+                            src={'/img/user.jpg'}
+                            alt={'user'}
+                            size="md"
+                            variant="circular"
+                            className={`cursor-pointer border-2 border-white`}
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="flex flex-row">
+                          <Avatar
+                            src={'/img/bot.jpg'}
+                            alt={'user'}
+                            size="xl"
+                            variant="circular"
+                            className={`cursor-pointer border-2 border-white`}
+                          />
+                          <div className="text-xl bg-gray-200 mt-2 p-3 rounded-r-3xl rounded-bl-3xl flex flex-row">
+                            <div>
+                              <AnimationMessage text= {value.answer} />
+                            </div>
+                            <IconButton
+                              variant="text"
+                              size="lg"
+                              onClick={() => copyText(value.answer)}
+                            >
+                              <i className="fa-solid fa-copy"></i>
+                            </IconButton >
+                          </div>
+
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+          </div>
+          <div className="flex flex-row">
+            <div className="basis-11/12 relative">
+              <Input
+                className="aboslute"
+                type="text"
+                label="Question"
+                size="lg"
+                disabled={loading}
+                value={question}
+                onChange={e => setQuestion(e.target.value)}
+                onKeyDown={e => e.keyCode == 13 ? handleSubmit(e, "") : ""}
+              />
+              <button
+                className="bg-blue-gray-800 rounded-3xl border-l-[1px] text-white text-center p-2 absolute top-0.5 right-2"
+                onClick={handleMic}
+              >
+                <i className="fas fa-microphone" />
+              </button>
+            </div>
+            <div className="basis-1/12 px-2 flex pb-3 items-end">
+              <Button variant="gradient" className={loading ? "cursor-not-allowed bg-blue-500 h-full rounded-l-3xl text-white text-center" : "bg-blue-500 rounded-l-3xl text-white text-center"} fullWidth onClick={(e) => handleSubmit(e, "")}>
+                Send
+              </Button>
+              <Menu className="h-full">
+                <MenuHandler>
+                  <button className="bg-blue-500 rounded-r-3xl border-l-[1px] text-white text-center p-2">
+                    <i className="fas fa-arrow-down" />
+                  </button>
+                </MenuHandler>
+                <MenuList>
+                  <MenuItem
+                    onClick={(e) => handleSubmit(e, "Correct Write")}
+                  >
+                    Grammer
+                  </MenuItem>
+
+                  <MenuItem
+                    onClick={(e) => handleSubmit(e, "Explain it")}
+                  >
+                    Explain
+                  </MenuItem>
+                  <MenuItem
+                    onClick={(e) => handleSubmit(e, "Create a list of 8 questions for my interview with a ")}
+                  >
+                    Interview
+                  </MenuItem>
+                  <MenuItem
+                    onClick={(e) => handleSubmit(e, "Translate to chinese")}
+                  >
+                    Chinese
+                  </MenuItem>
+                  <MenuItem
+                    onClick={(e) => handleSubmit(e, "Translate to english")}
+                  >
+                    English
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+            </div>
+          </div>
+        </CardBody>
+      </Card>
+
+    </div>
+  );
+}
+
+export default Chatbot;
---
+++ src/widgets/layout/sidenav.jsx
@@ -0,0 +1,137 @@
+import PropTypes from "prop-types";
+import { Link, NavLink } from "react-router-dom";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  Avatar,
+  Button,
+  IconButton,
+  Typography,
+} from "@material-tailwind/react";
+import { useMaterialTailwindController, setOpenSidenav } from "@/context";
+
+export function Sidenav({ brandImg, brandName, routes }) {
+  const [controller, dispatch] = useMaterialTailwindController();
+  const { sidenavColor, sidenavType, openSidenav } = controller;
+  const sidenavTypes = {
+    dark: "bg-gradient-to-br from-blue-gray-800 to-blue-gray-900",
+    white: "bg-white shadow-lg",
+    transparent: "bg-transparent",
+  };
+
+  return (
+    <aside
+      className={`${sidenavTypes[sidenavType]} ${openSidenav ? "translate-x-0" : "-translate-x-80"
+        } fixed inset-0 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 rounded-xl transition-transform duration-300 xl:translate-x-0`}
+    >
+      <div
+        className={`relative border-b ${sidenavType === "dark" ? "border-white/20" : "border-blue-gray-50"
+          }`}
+      >
+        <Link to="/" className="flex items-center gap-4 py-6 px-8">
+          <Avatar src={brandImg} size="sm" />
+          <Typography
+            variant="h6"
+            color={sidenavType === "dark" ? "white" : "blue-gray"}
+          >
+            {brandName}
+          </Typography>
+        </Link>
+        <IconButton
+          variant="text"
+          color="white"
+          size="sm"
+          ripple={false}
+          className="absolute right-0 top-0 grid rounded-br-none rounded-tl-none xl:hidden"
+          onClick={() => setOpenSidenav(dispatch, false)}
+        >
+          <XMarkIcon strokeWidth={2.5} className="h-5 w-5 text-white" />
+        </IconButton>
+      </div>
+      <div className="m-4">
+        {routes.map(({ layout, title, pages }, key) => (
+          <ul key={key} className="flex flex-col gap-1">
+            {title && title != "auth pages" && (
+              <li className="mx-3.5 mt-4 mb-2">
+                <Typography
+                  variant="small"
+                  color={sidenavType === "dark" ? "white" : "blue-gray"}
+                  className="font-black uppercase opacity-75"
+                >
+                  {title}
+                </Typography>
+              </li>
+            )}
+            {title != "auth pages" && pages.map(({ icon, name, path }) => (
+              <li key={name}>
+                <NavLink to={`/${layout}${path}`}>
+                  {({ isActive }) => (
+                    <Button
+                      variant={isActive ? "gradient" : "text"}
+                      color={
+                        isActive
+                          ? sidenavColor
+                          : sidenavType === "dark"
+                            ? "white"
+                            : "blue-gray"
+                      }
+                      className="flex items-center gap-4 px-4 py-2 capitalize"
+                      fullWidth
+                    >
+                      {icon}
+                      <Typography
+                        color="inherit"
+                        className="font-medium capitalize"
+                      >
+                        {name}
+                      </Typography>
+                    </Button>
+                  )}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        ))}
+      </div>
+      <div className="mx-4">
+        <p className="mx-6 text-white">{"Yesterday"}</p>
+        <div className="flex flex-col my-2 cursor-pointer">
+          <div className="px-4 py-2 bg-gradient-to-r from-blue-400 to-blue-300 rounded-md text-white relative">
+            <div className="flex gap-2 items-center">
+              <i className="fas fa-comments" />
+              <p>{"Initial greeting and"}</p>
+            </div>
+            <div className="absolute right-1 top-1 flex gap-1">
+              <IconButton className="rounded-full bg-blue-400" size="sm">
+                <i className="fas fa-edit" />
+              </IconButton>
+              <IconButton className="rounded-full bg-blue-400" size="sm">
+                <i className="fas fa-trash" />
+              </IconButton>
+            </div>
+          </div>
+          <div className="px-4 py-2 rounded-md text-white relative">
+            <div className="flex gap-2 items-center">
+              <i className="fas fa-comments" />
+              <p>{"Initial greeting and"}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </aside>
+  );
+}
+
+Sidenav.defaultProps = {
+  brandImg: "/img/logo-ct.png",
+  brandName: "Custom GPT",
+};
+
+Sidenav.propTypes = {
+  brandImg: PropTypes.string,
+  brandName: PropTypes.string,
+  routes: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+Sidenav.displayName = "/src/widgets/layout/sidnave.jsx";
+
+export default Sidenav;
